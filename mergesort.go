b9836f68bd1e0013e8cc57995a999832feab85f5
Alexei Danchenkov <adanchenkov@gmail.com>
2015-03-24 12:54:07 -0700

    Programming Assignment

--- mergesort.go
+++ mergesort.go
@@ -1,32 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
-// var Inversions int = 0
+var Inversions int = 0
+
+func readLines(path string) ([]int64, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		nextInt, err := strconv.ParseInt(scanner.Text(), 0, 64)
+		if err != nil {
+			return nil, err
+		}
+		lines = append(lines, nextInt)
+	}
+	return lines, scanner.Err()
+}
 
 func main() {
-	// original := []float64{2, 5, -2, 6, -3, 8, 0, -7, -9, 45, 3, 32, -16, 1, 9, 81, 7, 4}
-	original := []float64{1, 3, 5, 2, 4, 6}
-	a := make([]float64, 0, len(original))
-	b := make([]float64, 0, len(original))
+	// original := []int{2, 5, -2, 6, -3, 8, 0, -7, -9, 45, 3, 32, -16, 1, 9, 81, 7, 4}
+	// original := []int64{1, 3, 5, 2, 4, 6}
+	original, _ := readLines("IntegerArray.txt")
 
-	fmt.Printf("MERGE SORT ALGORITHM\n====================\nORIGINAL ARRAY: %v\n", original)
-	// Split
+	a := make([]int64, 0, len(original))
+	b := make([]int64, 0, len(original))
+
+	// fmt.Printf("MERGE SORT ALGORITHM\n====================\nORIGINAL ARRAY: %v\n", original)
+	// Splits
 	a = original[:len(original)/2]
 	// Split
 	b = original[len(original)/2:]
 	// fmt.Printf("A: %v\n", a)
 	// fmt.Printf("B: %v\n", b)
 	// Merge
-	sorted_array := MergeSort(a, b)
-	fmt.Printf("SORTED ARRAY:   %v\n", sorted_array)
-	// fmt.Printf("INVERSIONS: %d\n", Inversions)
+	MergeSort(a, b)
+	// fmt.Printf("SORTED ARRAY:   %v\n", sorted_array)
+	fmt.Printf("INVERSIONS: %d\n", Inversions)
 }
 
-func MergeSort(a, b []float64) []float64 {
-	c := make([]float64, 0, len(a)+len(b))
+func MergeSort(a, b []int64) []int64 {
+	c := make([]int64, 0, len(a)+len(b))
 	// Base case
 	if len(a) == 0 {
 		copy(c, b)
@@ -59,7 +83,8 @@ func MergeSort(a, b []float64) []float64 {
 				return c
 			}
 		} else {
-			fmt.Print(".")
+			// fmt.Print(".")
+			Inversions += len(a)
 			c = append(c, b[0])
 			// fmt.Printf("%v <= %v <= %v %v\n", c, b[0], b, a)
 			b = b[1:]
